0772d425f7aac8edc5f4f233a62085b7d814582d
Rio Pacit <rio.pacit@wework.com>
2018-02-13 18:46:34 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,7 @@
 # code the #greeting method here!
-puts "Hi! I'm HAL, what's your name?"
 
 def greeting(name)
+  puts "Hi! I'm HAL, what's your name?"
   puts "Hello #{name}. It's nice to meet you."
 end
 
